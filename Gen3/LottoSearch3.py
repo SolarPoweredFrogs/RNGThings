63e2823742e439d0384e8b811e1db2f2d1df0280
SolarPoweredFrogs <144302263+SolarPoweredFrogs@users.noreply.github.com>
2023-09-06 23:02:39 -0600

    Rename and cleanup

--- Gen3/LottoSearch3.py
+++ Gen3/LottoSearch3.py
@@ -1,14 +1,12 @@
+from RNGFunctions import RNGAdvance
+from RNGFunctions import H16
+
 TID = int(input('Enter your target ID: '))
 MaxDay = int(input('Max number of days: '))
 MinFrame = 100
 MaxFrame = int(input('Max number of advancements: '))
 
 Seed = 0
-
-def RNGAdvance(S): 
-	MULT = 0x41c64e6d
-	ADD = 0x6073
-	return (S * MULT + ADD) % 0x100000000
 
 for i in range(0,MinFrame):
 	Seed = RNGAdvance(Seed)
@@ -17,10 +15,6 @@ def LRNGAdvance(S):
 	MULT = 0x4e6d
 	ADD = 12345
 	return (S * MULT + ADD) % 0x10000
-
-
-def H16(x): 
-	return x >> 16
 
 def LottoDayAdvance(frame,seed,MaxDay,target):
 	LSeed = H16(seed)
